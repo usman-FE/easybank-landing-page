d46a23513b1b7326a3ca0316a1fb9e472f5cf16c
Usman-FE <ch.usman564@gmail.com>
2022-05-19 08:22:05 +0500

    Fix navigation toggler

--- app/js/script.js
+++ app/js/script.js
@@ -1,8 +1,18 @@
 const toggleBtn = document.querySelector(".navbar-toggler");
-const modal = document.querySelector("#exampleModal");
+const modal = document.querySelector(".modal");
 
 toggleBtn.addEventListener("click", function () {
-  console.log("button click");
+  if (toggleBtn.classList.contains("hamburger")) {
+    toggleBtn.classList.remove("hamburger");
+    toggleBtn.classList.add("close");
+  } else {
+    toggleBtn.classList.add("hamburger");
+    toggleBtn.classList.remove("close");
+  }
+});
+
+modal.addEventListener("click", function (e) {
+  if (!e.target.classList.contains("modal")) return;
   if (toggleBtn.classList.contains("hamburger")) {
     toggleBtn.classList.remove("hamburger");
     toggleBtn.classList.add("close");
